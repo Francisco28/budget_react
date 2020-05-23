be1c2f75f7ff9dcd521db582436fa7e1c913b46c
Francisco <fgallegosgo@gmail.com>
2020-05-22 19:52:34 -0600

    add state of 'budget'

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Question = () => {
+
+    //define the "state"
+    const [amount, saveAmount ] = useState(0);
+
+    //function that read the budget
+    const defineBudget = e => {
+        saveAmount(parseInt(e.target.value, 10));
+    }
+
+    //submit for define budget
+    const addBudget = e => {
+        e.preventDefault(); //no envie el query en la url y tampoco recargue la pagina
+        
+        //validate
+
+        //if pass the validate
+    }
+
     return ( 
         <Fragment>
             <h2>Put your budget</h2>
 
-            <form>
+            <form
+                onSubmit={ addBudget }
+            >
                 <input 
                     type="number"
                     className="u-full-width"
                     placeholder="Put your budget"
+                    onChange={ defineBudget }
                 />
 
                 <input 
