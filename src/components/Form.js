5446bbcc61da22e5eb1a82bf02f22309be26fb33
francisco28 <fgallegosgo@gmail.com>
2020-05-25 10:59:29 -0600

    state for expenses

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,30 @@
 import React, { useState } from 'react';
 
 const Form = () => {
+
+    //states - Hooks
+    const [name, saveName] = useState('');
+    const [amount, saveAmount] = useState(0);
+
+    
+    //when the user add an expense
+    const addExpense = e => {
+        e.preventDefault();
+        
+        //validate
+
+        //build an expense
+
+        //pass the expense to main component
+
+        //reset the form
+    }
+    
+
     return ( 
-        <form>
+        <form
+            onSubmit={addExpense}
+        >
             <h2>Add your expenses here</h2>
 
             <div className="campo">
@@ -11,6 +33,8 @@ const Form = () => {
                     type="text"
                     className="u-full-width"
                     placeholder="Ex. Transport"
+                    value={name}
+                    onChange={e => saveName(e.target.value)}
                 />
             </div>
 
@@ -20,6 +44,8 @@ const Form = () => {
                     type="number"
                     className="u-full-width"
                     placeholder="Ex. 300"
+                    value={amount}
+                    onChange={e => saveAmount(parseInt(e.target.value, 10))}
                 />
             </div>
 
